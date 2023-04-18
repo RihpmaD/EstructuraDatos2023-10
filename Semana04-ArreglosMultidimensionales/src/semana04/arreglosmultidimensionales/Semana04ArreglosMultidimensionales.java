a5b45a4d21e77fc1663489513dd6ebf713e24dcc
RihpmaD <c20177@utp.edu.pe>
2023-04-18 17:42:51 -0500

    Modificado
    Corregido

--- Semana04-ArreglosMultidimensionales/src/semana04/arreglosmultidimensionales/Semana04ArreglosMultidimensionales.java
+++ Semana04-ArreglosMultidimensionales/src/semana04/arreglosmultidimensionales/Semana04ArreglosMultidimensionales.java
@@ -55,15 +55,40 @@ public class Semana04ArreglosMultidimensionales {
         }
         
         for (int i = 0; i < datos.length; i++) {
-            System.out.println("\nTemporada "+(i+1));
             for (int j = 0; j < datos[0].length; j++) {
-                System.out.println("\nPartidos "+(j+1));
-                for (int k = 0; k < datos[0][0].length; k++) {
-                    System.out.println("\nGoles "+(k+1));
-                    System.out.println(datos[i][j][k]);
-                }
+                //for (int k = 0; k < datos[0][0].length; k++) {
+                    switch (i) {
+                        case 0:
+                            System.out.println("\t\tTEMPORADA ENERO-JULIO");
+                            temporadas[i]="Enero-Julio";
+                            break;
+                        case 1:
+                            System.out.println("\t\tTEMPORADA AGOSTO-DICIEMBRE");
+                            temporadas[i]="Agosto-Diciembre";
+                            break;
+                    }
+                    switch (j) {
+                        case 0:
+                            System.out.println("\tPARTIDO CON UNIVERSITARIO");
+                            equipos[j]="Universitario";
+                            break;
+                        case 1:
+                            System.out.println("\tPARTIDO CON ALIANZA LIMA");
+                            equipos[j]="Alianza Lima";
+                            break;
+                        case 2:
+                            System.out.println("\tPARTIDO CON CRISTAL");
+                            equipos[j]="Cristal";
+                            break;
+                    }
+                    
+                    System.out.println("\nREGISTRO DE ANOTACIONES\n");
+                    System.out.print("Goles anotados: "+datos[i][j][0]);
+                    System.out.print("Goles recibidos: "+datos[i][j][1]);
+                //}
             }
         }
+        
     }
 
 }
