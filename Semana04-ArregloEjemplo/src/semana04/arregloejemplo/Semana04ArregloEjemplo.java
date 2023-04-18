4b3d3086fde54b01e3426df1ffde09906e288eec
RihpmaD <c20177@utp.edu.pe>
2023-04-18 16:06:03 -0500

    Finalizado
    Ejemplo en clases

---
+++ Semana04-ArregloEjemplo/src/semana04/arregloejemplo/Semana04ArregloEjemplo.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package semana04.arregloejemplo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author billy
+ */
+public class Semana04ArregloEjemplo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x[] = new int[5];
+        int z[] = x;
+        for (int i = 0; i < x.length; i++) {
+            x[i] = LeerDato(i + 1);
+        }
+        System.out.println("Arreglo X\n");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(x[i] + ", ");
+        }
+        for (int i = 0; i < 5; i++) {
+            x[i] += 5;
+        }
+        System.out.println("\n\nArreglo Z\n");
+        for (int i = 0; i < 5; i++) {
+            System.out.print(z[i] + ", ");
+        }
+
+    }
+
+    public static int LeerDato(int valor) {
+        int a;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese un valor " + valor + ": ");
+        a = sc.nextInt();
+        return a;
+    }
+
+}
