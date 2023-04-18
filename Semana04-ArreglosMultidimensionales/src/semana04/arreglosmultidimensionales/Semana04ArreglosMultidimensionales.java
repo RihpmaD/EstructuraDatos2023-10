8e4f287cdc0ab0fa64d8e5aeaeef9ed425f9d719
RihpmaD <c20177@utp.edu.pe>
2023-04-18 18:05:43 -0500

    Finalizado
    Comentado los resultados

--- Semana04-ArreglosMultidimensionales/src/semana04/arreglosmultidimensionales/Semana04ArreglosMultidimensionales.java
+++ Semana04-ArreglosMultidimensionales/src/semana04/arreglosmultidimensionales/Semana04ArreglosMultidimensionales.java
@@ -13,6 +13,7 @@ public class Semana04ArreglosMultidimensionales {
         //3 - Partidos a equipos (0 Universitario, 1 Alianza Lima, 2 Cristal)
         //2 - Goles (Columna 1 - Anotados, Columna 2 - Recibidos)
         int datos[][][] = new int[2][3][2];
+        double promedio=0;
         String equipos[]=new String[3];
         String temporadas[]=new String[2];
         Scanner sc = new Scanner(System.in);
@@ -22,11 +23,11 @@ public class Semana04ArreglosMultidimensionales {
                 //for (int k = 0; k < datos[0][0].length; k++) {
                     switch (i) {
                         case 0:
-                            System.out.println("\t\tTEMPORADA ENERO-JULIO");
+                            System.out.println("\tTEMPORADA ENERO-JULIO");
                             temporadas[i]="Enero-Julio";
                             break;
                         case 1:
-                            System.out.println("\t\tTEMPORADA AGOSTO-DICIEMBRE");
+                            System.out.println("\tTEMPORADA AGOSTO-DICIEMBRE");
                             temporadas[i]="Agosto-Diciembre";
                             break;
                     }
@@ -54,40 +55,44 @@ public class Semana04ArreglosMultidimensionales {
             }
         }
         
-        for (int i = 0; i < datos.length; i++) {
+        System.out.println("\n\nRESUMEN DE RESULTADOS\n");
+        System.out.println("PROMEDIO DE GOLES");
+        for(int i=0;i<datos.length;i++){
+            for(int j=0;j<datos[0].length;j++){
+                promedio+=(datos[i][j][0]+datos[i][j][1]);
+            }
+            promedio/=datos[0].length;
+            System.out.printf("TEMPORADA %s: %.2f %n", temporadas[i], promedio);
+        }
+        /*for (int i = 0; i < datos.length; i++) {
             for (int j = 0; j < datos[0].length; j++) {
                 //for (int k = 0; k < datos[0][0].length; k++) {
                     switch (i) {
                         case 0:
-                            System.out.println("\t\tTEMPORADA ENERO-JULIO");
-                            temporadas[i]="Enero-Julio";
+                            System.out.println("\tTEMPORADA ENERO-JULIO");
                             break;
                         case 1:
-                            System.out.println("\t\tTEMPORADA AGOSTO-DICIEMBRE");
-                            temporadas[i]="Agosto-Diciembre";
+                            System.out.println("\tTEMPORADA AGOSTO-DICIEMBRE");
                             break;
                     }
                     switch (j) {
                         case 0:
                             System.out.println("\tPARTIDO CON UNIVERSITARIO");
-                            equipos[j]="Universitario";
                             break;
                         case 1:
                             System.out.println("\tPARTIDO CON ALIANZA LIMA");
-                            equipos[j]="Alianza Lima";
                             break;
                         case 2:
                             System.out.println("\tPARTIDO CON CRISTAL");
-                            equipos[j]="Cristal";
                             break;
                     }
                     
                     System.out.println("\nREGISTRO DE ANOTACIONES\n");
-                    System.out.print("Goles anotados: "+datos[i][j][0]);
-                    System.out.print("Goles recibidos: "+datos[i][j][1]);
+                    System.out.println("Goles anotados: "+datos[i][j][0]);
+                    System.out.println("Goles recibidos: "+datos[i][j][1]);
                 //}
             }
-        }
+        }*/
         
     }
 
