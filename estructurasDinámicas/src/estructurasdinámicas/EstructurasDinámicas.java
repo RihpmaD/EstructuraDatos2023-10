375d4d9df7115f361e6b45acad3e3204bd30bf7b
RihpmaD <c20177@utp.edu.pe>
2023-05-09 15:15:27 -0500

    Finalizado
    Poner en práctica

--- estructurasDinámicas/src/estructurasdinámicas/EstructurasDinámicas.java
+++ estructurasDinámicas/src/estructurasdinámicas/EstructurasDinámicas.java
@@ -1,4 +1,3 @@
-
 package estructurasdinámicas;
 
 import java.util.Scanner;
@@ -9,22 +8,78 @@ import listas.ListaSimpleEntero;
  * @author C20177
  */
 public class EstructurasDinámicas {
-    
+
     public static void main(String[] args) {
-        ListaSimpleEntero lista=new ListaSimpleEntero();
-        Scanner sc=new Scanner(System.in);
-        int valor;
+        ListaSimpleEntero lista = new ListaSimpleEntero();
+        Scanner sc = new Scanner(System.in);
+        int valor, op, elemento;
         char opc;
-        System.out.println("MANIPULACIÓN DE LISTAS\n");
-        do{
-            System.out.print("Ingrese valor: ");
-            valor=sc.nextInt();
-            lista.insertarCola(valor);
-            System.out.print("Desea ingresar otro elemento: ");
-            opc=sc.next().toUpperCase().charAt(0);
-        }while(opc!='N');
-        System.out.println("\nLISTA INGRESADA");
-        lista.mostrarLista();
+        do {
+            System.out.println("\nMANIPULACIÓN DE LISTAS\n");
+            System.out.println("1 - Ingresar valores");
+            System.out.println("2 - Ingresar en posicion");
+            System.out.println("3 - Mostrar lista");
+            System.out.println("4 - Insertar antes de un elemento");
+            System.out.println("5 - Insertar despues de un elemento");
+            System.out.println("6 - Eliminar elemento");
+            System.out.println("7 - Eliminar posicion");
+            System.out.println("8 - Salir");
+            System.out.println("----------------------------");
+            System.out.print("Ingrese una opcion: ");
+            op = sc.nextInt();
+            switch (op) {
+                case 1:
+                    do {
+                        System.out.print("Ingrese valor: ");
+                        valor = sc.nextInt();
+                        lista.insertarCola(valor);
+                        System.out.print("Desea ingresar otro elemento: ");
+                        opc = sc.next().toUpperCase().charAt(0);
+                    } while (opc != 'N');
+                    break;
+                case 2:
+                    int posicion;
+                    System.out.print("Ingrese valor: ");
+                    valor = sc.nextInt();
+                    System.out.print("Ingrese posicion: ");
+                    posicion = sc.nextInt();
+                    lista.insertarposicion(posicion, valor);
+                    break;
+                case 3:
+                    System.out.println("\nLISTA INGRESADA");
+                    lista.mostrarLista();
+                    break;
+                case 4:
+                    System.out.print("Ingrese valor del elemento a buscar: ");
+                    elemento = sc.nextInt();
+                    System.out.print("Ingrese el valor a insertar: ");
+                    valor=sc.nextInt();
+                    lista.insertarAntesElemento(elemento, valor);
+                    break;
+                case 5:
+                    System.out.print("Ingrese valor del elemento a buscar: ");
+                    elemento = sc.nextInt();
+                    System.out.print("Ingrese el valor a insertar: ");
+                    valor=sc.nextInt();
+                    lista.insertarDespuesElemento(elemento, valor);
+                    break;
+                case 6:
+                    System.out.print("Ingrese el valor a eliminar: ");
+                    valor=sc.nextInt();
+                    lista.eliminarElemento(valor);
+                    break;
+                case 7:
+                    System.out.print("Ingrese posicion a eliminar: ");
+                    posicion=sc.nextInt();
+                    lista.eliminarPosicion(posicion);
+                case 8:
+                    System.out.println("Gracias por usar el sistema...");
+                    break;
+                default:
+                    System.out.println("Opcion ingresada incorrecta...");
+            }
+        } while (op != 8);
+
     }
-    
+
 }
--- estructurasDinámicas/src/listas/ListaSimpleEntero.java
+++ estructurasDinámicas/src/listas/ListaSimpleEntero.java
@@ -28,7 +28,7 @@ public class ListaSimpleEntero {
             NodoSimpleEntero nuevo = new NodoSimpleEntero(valor);
             this.cabeza = nuevo;
         } else {
-            NodoSimpleEntero nuevo = new NodoSimpleEntero(valor, cabeza);
+            NodoSimpleEntero nuevo = new NodoSimpleEntero(valor, this.cabeza);
             this.cabeza = nuevo;
         }
         this.tam++;
@@ -46,15 +46,197 @@ public class ListaSimpleEntero {
             }
             temp.setSiguiente(nuevo);
         }
+        this.tam++;
+    }
+
+    public void insertarposicion(int posicion, int valor) {
+        int contador = 0;
+        if (esVacio() || posicion <= 0 || posicion > this.tam) {
+            if (posicion == 0) {
+                this.insertarCabeza(valor);
+                System.out.println("Se inserto en la cabeza...");
+                this.tam++;
+            } else {
+                System.out.println("No se puede insertar el elemento en la posicion " + posicion);
+            }
+        } else {
+            NodoSimpleEntero nuevo = new NodoSimpleEntero(valor);
+            NodoSimpleEntero temp = this.cabeza;
+            NodoSimpleEntero actual = this.cabeza;
+            NodoSimpleEntero siguiente = this.cabeza;
+            while (contador != posicion) {
+                actual = temp;
+                siguiente = temp.getSiguiente();
+                temp = temp.getSiguiente();
+                contador++;
+            }
+            actual.setSiguiente(nuevo);
+            nuevo.setSiguiente(siguiente);
+            this.tam++;
+        }
+    }
+
+    public void insertarAntesElemento(int elemento, int valor) {
+        if (esVacio()) {
+            System.out.println("No existe elementos en la lista...");
+        } else {
+            if (this.buscarElementoEstado(elemento)) {
+                NodoSimpleEntero nuevo = new NodoSimpleEntero(valor);
+                NodoSimpleEntero anterior = this.buscarElementoAnterior(elemento);
+                NodoSimpleEntero temp = this.buscarElemento(elemento);
+                if (temp == this.cabeza) {
+                    nuevo.setSiguiente(this.cabeza);
+                    this.cabeza = nuevo;
+                } else {
+                    anterior.setSiguiente(nuevo);
+                    nuevo.setSiguiente(temp);
+                }
+                this.tam++;
+            } else {
+                System.out.println("Elemento no encontrado en la lista...");
+            }
+        }
+    }
+
+    public void insertarDespuesElemento(int elemento, int valor) {
+        if (esVacio()) {
+            System.out.println("No existe elementos en la lista...");
+        } else {
+            if (this.buscarElementoEstado(elemento)) {
+                NodoSimpleEntero nuevo = new NodoSimpleEntero(valor);
+                NodoSimpleEntero temp = this.buscarElemento(elemento);
+                if (temp.getSiguiente() != null) {
+                    nuevo.setSiguiente(temp.getSiguiente());
+                    temp.setSiguiente(nuevo);
+                } else {
+                    temp.setSiguiente(nuevo);
+                }
+                this.tam++;
+            } else {
+                System.out.println("Elemento no encontrado en la lista...");
+            }
+        }
+
+    }
+
+    public void eliminarElemento(int valor) {
+        if (esVacio()) {
+            System.out.println("Lista vacia, no se puede eliminar elemento...");
+        } else {
+            if (this.buscarElementoEstado(valor)) {
+                NodoSimpleEntero elemento = this.buscarElemento(valor);
+                NodoSimpleEntero anterior = this.buscarElementoAnterior(valor);
+                if (elemento == this.cabeza) {
+                    this.cabeza = elemento.getSiguiente();
+                } else if (elemento.getSiguiente() == null) {
+                    anterior.setSiguiente(null);
+                } else {
+                    anterior.setSiguiente(elemento.getSiguiente());
+                }
+                this.tam--;
+            } else {
+                System.out.println("Elemento no encontrado en la lista...");
+            }
+        }
+    }
+    
+    public void eliminarPosicion(int posicion){
+        if (esVacio()) {
+            System.out.println("Lista vacia, no se puede eliminar elemento...");
+        } else {
+            if (posicion<0 || posicion>this.tam) {
+                System.out.println("Posicion no existente en la lista...");
+            } else {
+                NodoSimpleEntero elemento = this.buscarPosicion(posicion);
+                NodoSimpleEntero anterior = this.buscarPosicionAnterior(posicion);
+                if (elemento == this.cabeza) {
+                    this.cabeza = elemento.getSiguiente();
+                } else if (elemento.getSiguiente() == null) {
+                    anterior.setSiguiente(null);
+                } else {
+                    anterior.setSiguiente(elemento.getSiguiente());
+                }
+                this.tam--;
+            }
+        }
+    }
+    
+    public NodoSimpleEntero buscarPosicion(int posicion){
+        int contador=0;
+        NodoSimpleEntero temp = this.cabeza;
+        while(contador<posicion){
+            temp=temp.getSiguiente();
+            contador++;
+        }
+        return temp;
+    }
+    
+    public NodoSimpleEntero buscarPosicionAnterior(int posicion){
+        int contador=0;
+        NodoSimpleEntero temp=this.cabeza;
+        NodoSimpleEntero anterior=null;
+        while(contador<posicion){
+            anterior=temp;
+            temp=temp.getSiguiente();
+            contador++;
+        }
+        return anterior;
+    }
+
+    public NodoSimpleEntero buscarElemento(int valor) {
+        NodoSimpleEntero temp = this.cabeza;
+        while (temp.getSiguiente() != null) {
+            if (temp.getValor() == valor) {
+                return temp;
+            }
+            temp = temp.getSiguiente();
+        }
+        if (temp.getValor() == valor) {
+            return temp;
+        } else {
+            return null;
+        }
+    }
+
+    public NodoSimpleEntero buscarElementoAnterior(int valor) {
+        NodoSimpleEntero temp = this.cabeza;
+        NodoSimpleEntero anterior = null;
+        while (temp.getSiguiente() != null) {
+            if (temp.getValor() == valor) {
+                return anterior;
+            }
+            anterior = temp;
+            temp = temp.getSiguiente();
+        }
+        if (temp.getValor() == valor) {
+            return anterior;
+        } else {
+            return null;
+        }
+    }
+
+    public boolean buscarElementoEstado(int valor) {
+        NodoSimpleEntero temp = this.cabeza;
+        while (temp.getSiguiente() != null) {
+            if (temp.getValor() == valor) {
+                return true;
+            }
+            temp = temp.getSiguiente();
+        }
+        return temp.getValor() == valor;
     }
 
     public void mostrarLista() {
-        NodoSimpleEntero temp = this.cabeza;
-        while (temp.getSiguiente() != null) {
+        if (esVacio()) {
+            System.out.println("Lista vacia...");
+        } else {
+            NodoSimpleEntero temp = this.cabeza;
+            while (temp.getSiguiente() != null) {
+                System.out.print(temp.getValor() + ", ");
+                temp = temp.getSiguiente();
+            }
             System.out.print(temp.getValor() + ", ");
-            temp = temp.getSiguiente();
+            System.out.println("");
         }
-        System.out.print(temp.getValor() + ", ");
-        System.out.println("");
     }
 }
